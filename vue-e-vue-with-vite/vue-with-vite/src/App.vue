3c90608aa77f5264d90403728564b9dd6e6b91fc
Franziska Kiel <kiel.franziska@outlook.com>
2025-02-19 16:32:08 +0100

    init

---
+++ vue-e-vue-with-vite/vue-with-vite/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <MainHeader />
+    <main>
+      <div id="app" class="inside">
+        <BookList />
+      </div>
+    </main>
+  </div>
+</template>
+<!-- 
+In Vue 3, a <template> must have only one root element. 
+This means that everything inside your <template> must be 
+wrapped in a single parent element, such as a <div>.
+
+Why is this needed?
+Vue’s virtual DOM works with a single tree structure. 
+If your template has multiple root elements, Vue 
+wouldn’t know which one represents the component’s 
+main structure. 
+-->
+
+<script>
+import MainHeader from './components/MainHeader.vue'
+import BookList from './components/BookList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MainHeader,
+    BookList,
+  },
+}
+</script>
